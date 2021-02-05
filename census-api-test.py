6b0937eec9b5590a95751c700e52d29309a1e5e5
Aaron Bager <kingjojo3@gmail.com>
2021-02-05 16:13:33 -0500

    Came up with method of pulling data automatically from census.gov

--- census-api-test.py
+++ census-api-test.py
@@ -1,8 +1,22 @@
+# Aaron Bager
+# Method for getting population of a single country; in this case, norway 
+
 import config # contains census API key, contact Aaron for key if needed
 import requests
+import json
 
 countries = []
 
-response = requests.get("https://api.census.gov/data/timeseries/idb/1year?get=NAME,AGE,POP,AREA_KM2&GENC=NO&YR=2021&SEX=0&key=" + config.census_key);
+response = requests.get("https://api.census.gov/data/timeseries/idb/1year?get=POP&GENC=NO&YR=2021&SEX=0&key=" + config.census_key);
 
-print(response.json())
+population = 0
+
+first_column = True
+for data in response.json():
+    if first_column:
+        first_column = False 
+        continue
+    population += int(data[0])
+#    print(data[3])
+
+print(population) 
